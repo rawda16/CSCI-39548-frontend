0c52fc599fbfcf40fe798e605c843491aca99f40
Lindsay Beauliere <beaulierelindsay@gmail.com>
2025-12-08 18:03:49 -0500

    handling viewing of posts

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import { PostCard, PostList } from "./components";
+
+function App() {
+   const posts = [
+      {
+         id: "p1",
+         userId: "u1",
+         username: "maida",
+
+         title: "Mean Girls Woo",
+         description:
+            "Look at this chic outfit from Mean Girls!!Look at this chic outfit from Mean Girls!!Look at this chic outfit from Mean Girls!!Look at this chic outfit from Mean Girls!!Look at this chic outfit from Mean Girls!!Look at this chic outfit from Mean Girls!!Look at this chic outfit from Mean Girls!!",
+         periodTag: "2000s",
+         genreTag: "Comedy",
+         commentCount: 3,
+      },
+      {
+         id: "p2",
+         userId: "u2",
+         username: "rawda",
+
+         title: "Vamps Are Cool",
+         description: "Look at these victorian style fits from Dracula :O",
+         periodTag: "2000s",
+         genreTag: "Action",
+         commentCount: 5,
+      },
+   ];
+   return <PostList posts={posts} />;
+}
+
+export default App;
