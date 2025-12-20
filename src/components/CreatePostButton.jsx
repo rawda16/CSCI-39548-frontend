336c78ff23336962fb81b1c0dacef952d8273586
Lindsay Beauliere <beaulierelindsay@gmail.com>
2025-12-20 06:16:08 -0500

    empty user fix

--- src/components/CreatePostButton.jsx
+++ src/components/CreatePostButton.jsx
@@ -1,6 +1,8 @@
 import { Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 function CreatePostButton() {
+   const navigate = useNavigate();
    return (
       <Button
          variant="contained"
